fc0c727669ab0165cc49b906818eba3cdd13bc11
kvismaya <137600672+kvismaya@users.noreply.github.com>
2023-06-29 01:00:44 +0530

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,20 @@
+#LIST
+#finding intersection
+E={0,2,4,6,8}
+N={2,1,5,4,3}
+print('The intersection of E and N are: ')
+print(E.intersection(N))
+
+#finding unnion between two sets
+print('The union of E and N are: ')
+print(E.union(N))
+
+#difference between two sets 
+print('The difference of E and N are: ')
+print(E.difference(N))
+
+#symmetrical difference between two sets
+print('The symmetrical difference of E and N are: ')
+print(E.symmetric_difference(N))
+
+
